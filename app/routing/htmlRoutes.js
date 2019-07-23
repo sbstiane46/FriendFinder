f67cdbdd1420a49089c63a1a13498a8897829481
sbstiane46 <sbstian89@yahoo.com>
2019-07-23 17:35:00 -0400

    file rearrangement

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = function(app) {
     app.get('/survey', function (req,res) {
         res.sendFile(path.join(_dirname, '.public/survey.html'));
     });
-    // app.get('', function (req,res) {
-    //     res.sendFile(path.join(_dirname, '.public/home.html'));
-    // });
+    app.get('*', function (req,res) {
+        res.sendFile(path.join(_dirname, '.public/home.html'));
+    });
 };
