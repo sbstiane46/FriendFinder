816d7ad2aa06af88aa9c5073d2b33e74ee2faf0f
sbstiane46 <sbstian89@yahoo.com>
2019-07-25 16:18:09 -0400

    progress

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,4 +1,5 @@
-var surveyData = require("../data/friends");
+var surveyData = require("../data/friends.js");
+
 
 module.exports = function(app) {
     app.get('/api/friends', function(req, res) {
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,14 @@
 var express = require("express");
 var app = express();
-var path = require('path');
+var path = require("path");
 
 module.exports = function(app) {
-    app.get('/home', function (req,res) {
-        res.sendFile(path.join(_dirname, '.public/home.html'));
+
+    app.get("/", function (req,res) {
+        res.sendFile(path.join(_dirname, ".public/home.html"));
     });
-    app.get('/survey', function (req,res) {
-        res.sendFile(path.join(_dirname, '.public/survey.html'));
+    app.get("/survey", function (req,res) {
+        res.sendFile(path.join(_dirname, ".public/survey.html"));
     });
-    app.get('*', function (req,res) {
-        res.sendFile(path.join(_dirname, '.public/home.html'));
-    });
+    
 };
