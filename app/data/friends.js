4c25a32446ab11ccce44f1588b4b58566e4b2533
sbstiane46 <sbstian89@yahoo.com>
2019-07-25 15:38:57 -0400

    update

---
+++ app/data/friends.js
@@ -0,0 +1,57 @@
+var friendArray = [
+    {
+        name: "Bruno",
+        photo: "",
+        score: ["1","5","2","5","2"],
+    },
+    {
+        name: "Joli",
+        photo: "",
+        score: ["2","1","5","1","3"],
+    },
+    {
+        name: "Luna",
+        photo: "",
+        score: ["5","5","4","2","5"]
+    },
+    {
+        name: "Kingston",
+        photo: "",
+        score: ["3","2","5","5","4"]
+    },
+    {
+        name: "Pancho",
+        photo: "",
+        score: ["1","4","3","5","1"]
+    },
+    {
+        name: "Menta",
+        photo: "",
+        score: ["3","2","5","2","1"],
+    },
+    {
+        name: "Canela",
+        photo: "",
+        score: ["3","3","3","2","5"],
+    },
+    {
+        name: "Winston",
+        photo: "",
+        score: ["1","3","3","5","2"]
+    },
+    {
+        name: "Melissa",
+        photo: "",
+        score: ["3","1","5","5","3"]
+    },
+    {
+        name: "Fender",
+        photo: "",
+        score: ["4","3","5","3","1"]
+];
+
+module.exports = friendArray;
+
+var scoreArray = [];
+
+module.exports = scoreArray;
