e9b9d806b5bebeb2acb4aeed4c24c4e692abb15c
sbstiane46 <sbstian89@yahoo.com>
2019-08-22 17:14:39 -0400

    images edited

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,52 +1,52 @@
 var friendArray = [
     {
         name: "Bruno",
-        photo: <img src="\dataimages\bruno.jpg"></img>,
+        photo: "/data/dataimages/bruno.jpg",
         score: ["1","5","2","5","2"],
     },
     {
         name: "Joli",
-        photo:  <img src="\dataimages\joli.jpg"></img>,
+        photo: "/data/dataimages/joli.jpg",
         score: ["2","1","5","1","3"],
     },
     {
         name: "Luna",
-        photo:  <img src="\dataimages\luna.jpg"></img>,
+        photo: "/data/dataimages/luna.jpg",
         score: ["5","5","4","2","5"]
     },
     {
         name: "Kingston",
-        photo:  <img src="\dataimages\kingston.jpg"></img>,
+        photo: "/data/dataimages/kingston.jpg",
         score: ["3","2","5","5","4"]
     },
     {
         name: "Pancho",
-        photo:  <img src="\dataimages\pancho.jpg"></img>,
+        photo: "/data/dataimages/pancho.jpg",
         score: ["1","4","3","5","1"]
     },
     {
         name: "Menta",
-        photo:  <img src="\dataimages\menta.jpg"></img>,
+        photo: "/data/dataimages/menta.jpg",
         score: ["3","2","5","2","1"],
     },
     {
         name: "Canela",
-        photo:  <img src="\dataimages\canela.jpg"></img>,
+        photo: "/data/dataimages/canela.jpg",
         score: ["3","3","3","2","5"],
     },
     {
         name: "Winston",
-        photo:  <img src="\dataimages\winston.jpg"></img>,
+        photo: "/data/dataimages/winston.jpg",
         score: ["1","3","3","5","2"]
     },
     {
         name: "Melissa",
-        photo:  <img src="\dataimages\melissa.jpg"></img>,
+        photo: "/data/dataimages/melissa.jpg",
         score: ["3","1","5","5","3"]
     },
     {
         name: "Fender",
-        photo:  <img src="\dataimages\fender.jpg"></img>,
+        photo: "/data/dataimages/fender.jpg",
         score: ["4","3","5","3","1"]
     }
     ];
